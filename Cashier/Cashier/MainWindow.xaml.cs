ad271c77c8d21f1ae53606594d6ac7eb7e273cf4
kaling <yujialingbbs@163.com>
2015-12-26 20:27:37 +0800

    搞定数据库，Mysql

--- Cashier/Cashier/MainWindow.xaml.cs
+++ Cashier/Cashier/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace Cashier
     /// </summary>
     public partial class MainWindow : Window
     {
+        //用户账户
+        private String user;
+        private String password;
         public MainWindow()
         {
             InitializeComponent();
@@ -38,10 +41,18 @@ namespace Cashier
         //监听注册按钮
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-
-            RegisterWindow register = new RegisterWindow();
-            register.Show();
-            this.Close();
+            Button btn = sender as Button;
+            String choice = btn.Content.ToString();
+            switch (choice)
+            {
+                case "登录":
+                    UserLogin();
+                    break;
+                case "注册":
+                    UserRegister();
+                    break;
+            }
+            
         
         }
 
@@ -49,5 +60,39 @@ namespace Cashier
         {
 
         }
+
+        private void UserLogin()
+        {
+            //用户登录的逻辑代码
+            user = userBox.Text.ToString();
+            password = passwordBox.Text.ToString();
+            if(user.Equals(""))
+            {
+                MessageBox.Show("请输入用户名");
+            }
+            else if(password.Equals(""))
+            {
+                MessageBox.Show("请输入密码");
+            }
+            else
+            {
+                CheckInfoAndLogin();
+            }
+        }
+
+        private void UserRegister()
+        {
+            //跳转到登陆界面
+            RegisterWindow register = new RegisterWindow();
+            register.Show();
+            this.Close();
+        }
+
+        //检查用户的数据，如果查询失败则返回密码错误
+        private void CheckInfoAndLogin()
+        {
+
+        }
+
     }
 }
--- Cashier/Cashier/RegisterWindow.xaml.cs
+++ Cashier/Cashier/RegisterWindow.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Configuration;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
 
 namespace Cashier
 {
@@ -19,6 +24,11 @@ namespace Cashier
     /// </summary>
     public partial class RegisterWindow : Window
     {
+
+        private String user;
+        private String password;
+        private String passwordAgain;
+
         public RegisterWindow()
         {
             InitializeComponent();
@@ -26,9 +36,68 @@ namespace Cashier
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            user = userBox.Text.ToString();
+            password = passwordBox.Text.ToString();
+            passwordAgain = passwordAgainBox.Text.ToString();
+            Button btn = sender as Button;
+            String choice = btn.Content.ToString();
+            switch (choice)
+            {
+                case "注册":
+                    UserRegister();
+                    break;
+                case "登录":
+                    UserLogin();
+                    break;
+            }
+        }
+
+
+        private void UserRegister()
+        {
+
+            if (user.Equals(""))
+            {
+                MessageBox.Show("请输入用户名");
+            }
+            else if (password.Equals(""))
+            {
+                MessageBox.Show("请输入密码");
+            }
+            else if (!password.Equals(passwordAgain))
+            {
+                MessageBox.Show("请检查两次输入的密码是否正确");
+            }
+            else
+            {
+                CheckAndRegister();
+            }
+        }
+
+        //转到用户登录界面
+        private void UserLogin()
+        {
             MainWindow register = new MainWindow();
             register.Show();
             this.Close();
+        }
+
+        //验证用户信息， 如果不存在用户就并注册用户信息
+        private void CheckAndRegister()
+        {
+            String conStr = "server=localhost;User Id=root;password=;Database=canyin";
+            MySqlConnection myCon = new MySqlConnection(conStr);
+            myCon.Open();
+            String cmdtest = "insert into user(user_id, user, password, identity) values('', 'testuser', 'testpass', 1)";
+            MySqlCommand mycmd = new MySqlCommand(cmdtest, myCon);
+            if(mycmd.ExecuteNonQuery() > 0)
+            {
+                MessageBox.Show("成功");
+            }
+            else
+            {
+                MessageBox.Show("对不起，用户注册失败");
+            }
         }
     }
 }
